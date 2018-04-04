3fb8f2722069071c88f2aa6c841ba5b433d468b3
Jonathan Lau <jonhlau28@gmail.com>
2018-04-04 09:27:03 -0700

    finish logincontainer style

--- frontend/components/session/login_form.jsx
+++ frontend/components/session/login_form.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 class LoginForm extends React.Component {
   constructor(props) {
@@ -25,24 +25,33 @@ class LoginForm extends React.Component {
 
   render() {
     return(
-      <div>
-        <h2>Login</h2>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            placeholder="Email"
-            value={this.state.email}
-            onChange={this.handleChange('email')}
-          />
-
-          <input
-            type="password"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={this.handleChange('password')}
-          />
-        <input type="submit" value="Sign In"/>
-        </form>
+      <div className="session-box-body">
+        <div className="session-box">
+          <img className="login-logo" alt="logo-img" src="https://s.yimg.com/rz/d/yahoo_en-US_f_p_bestfit_2x.png" />
+          <h2><p>Sign in</p></h2>
+          <form className="login-form-challenge" onSubmit={this.handleSubmit}>
+            <div className="login-input-field-box">
+              <input
+                id="login-email-input"
+                type="text"
+                placeholder="Enter your email"
+                value={this.state.email}
+                onChange={this.handleChange('email')}
+              />
+            </div>
+            <div className="login-input-field-box">
+              <input
+                id="login-password-input"
+                type="password"
+                placeholder="Password"
+                value={this.state.password}
+                onChange={this.handleChange('password')}
+              />
+            </div>
+          <input className="signInButton" id="login-sigin" type="submit" name="signin" value="Sign in"/>
+          <div className="row-sign-up">Don't have an acccount? <Link to="/signup"><p id="sign-up-link">Sign up</p></Link></div>
+          </form>
+        </div>
       </div>
     );
   }
--- frontend/components/session/sign_up.jsx
+++ frontend/components/session/sign_up.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class SignupForm extends React.Component {
   constructor(props) {
@@ -27,41 +28,48 @@ class SignupForm extends React.Component {
 
   render() {
     return (
-      <div>
-        <h3>Sign Up</h3>
-          <form onSubmit={this.handleSubmit}>
+      <div className="session-box-body">
+        <div className="session-box">
+          <h2><p>Sign up</p></h2>
+            <form onSubmit={this.handleSubmit}>
+              <div className="first-name-contain">
+                <input
+                  type="text"
+                  placeholder="First name"
+                  value={this.state.fname}
+                  onChange={this.handleChange('fname')}
+                />
+              </div>
+              <div className="last-name-contain">
+                <input
+                  type="text"
+                  placeholder="Last name"
+                  value={this.state.lname}
+                  onChange={this.handleChange('lname')}
+                />
+              </div>
             <input
-            type="text"
-            placeholder="First name"
-            value={this.state.fname}
-            onChange={this.handleChange('fname')}
-          />
-          <input
-            type="text"
-            placeholder="Last name"
-            value={this.state.lname}
-            onChange={this.handleChange('lname')}
-          />
-          <input
-            type="text"
-            placeholder="Email"
-            value={this.state.email}
-            onChange={this.handleChange('email')}
-          />
-          <input
-            type="text"
-            placeholder="Username"
-            value={this.state.username}
-            onChange={this.handleChange('username')}
-          />
-          <input
-              type="password"
-              placeholder="Password: Minimum 6 characters."
-              value={this.state.password}
-              onChange={this.handleChange('password')}
+              type="text"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={this.handleChange('email')}
             />
-           <input type="submit" value="Sign Up"/>
-          </form>
+            <input
+              type="text"
+              placeholder="Username"
+              value={this.state.username}
+              onChange={this.handleChange('username')}
+            />
+            <input
+                type="password"
+                placeholder="Password: Minimum 6 characters."
+                value={this.state.password}
+                onChange={this.handleChange('password')}
+              />
+             <input type="submit" value="Sign Up"/>
+             <div className="row-sign-in">Already have an acccount? <Link to="/login"><p id="log-in-link">Sign in</p></Link></div>
+            </form>
+          </div>
       </div>
     );
   }
