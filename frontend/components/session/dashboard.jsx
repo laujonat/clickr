602d4b30fb60c4e5d1b4808615d0f4fbf87994df
Jonathan Lau <jonhlau28@gmail.com>
2018-04-04 11:50:16 -0700

    add sessionContainer styling

--- frontend/components/session/dashboard.jsx
+++ frontend/components/session/dashboard.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const sessionLinks = () => (
-  <nav className="login-signup">
+  <div className="login-signup">
     <Link to="/login">Login</Link>
     <Link to="/signup">Sign up</Link>
-  </nav>
+  </div>
 );
 
 const personalGreeting = (currentUser, logout) => (
--- frontend/components/session/login_form.jsx
+++ frontend/components/session/login_form.jsx
@@ -25,33 +25,43 @@ class LoginForm extends React.Component {
 
   render() {
     return(
-      <div className="session-box-body">
-        <div className="session-box">
-          <img className="login-logo" alt="logo-img" src="https://s.yimg.com/rz/d/yahoo_en-US_f_p_bestfit_2x.png" />
-          <h2><p>Sign in</p></h2>
-          <form className="login-form-challenge" onSubmit={this.handleSubmit}>
-            <div className="login-input-field-box">
-              <input
-                id="login-email-input"
-                type="text"
-                placeholder="Enter your email"
-                value={this.state.email}
-                onChange={this.handleChange('email')}
-              />
-            </div>
-            <div className="login-input-field-box">
-              <input
-                id="login-password-input"
-                type="password"
-                placeholder="Password"
-                value={this.state.password}
-                onChange={this.handleChange('password')}
-              />
-            </div>
-          <input className="signInButton" id="login-sigin" type="submit" name="signin" value="Sign in"/>
-          <div className="row-sign-up">Don't have an acccount? <Link to="/signup"><p id="sign-up-link">Sign up</p></Link></div>
-          </form>
+      <div>
+        <header className="session-new-header">
+          <div className="header-logo-contain">
+            <img className="top-logo" alt="logo-img" src="https://s.yimg.com/rz/d/yahoo_en-US_f_p_bestfit_2x.png" />
+          </div>
+        </header>
+        <div className="login-position-container">
+          <div className="session-box-body">
+          <div className="session-box">
+            <img className="login-logo" alt="logo-img" src="https://s.yimg.com/rz/d/yahoo_en-US_f_p_bestfit_2x.png" />
+            <h2><p>Sign in</p></h2>
+            <form className="login-form-challenge" onSubmit={this.handleSubmit}>
+              <div className="login-input-field-box">
+                <input
+                  id="login-email-input"
+                  type="text"
+                  placeholder="Enter your email"
+                  value={this.state.email}
+                  onChange={this.handleChange('email')}
+                />
+              </div>
+              <div className="login-input-field-box">
+                <input
+                  id="login-password-input"
+                  type="password"
+                  placeholder="Password"
+                  value={this.state.password}
+                  onChange={this.handleChange('password')}
+                />
+              </div>
+            <input className="signInButton" id="login-sigin" type="submit" name="signin" value="Sign in"/>
+            <div className="row-sign-up">Don't have an acccount? <Link to="/signup"><p id="sign-up-link">Sign up</p></Link></div>
+            </form>
+          </div>
         </div>
+        </div>
+        <footer className="login-footer"></footer>
       </div>
     );
   }
