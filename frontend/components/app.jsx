6536b439530f6579eef81e2ae7d2b12ba0b964f3
Jonathan Lau <jonathan.lau@catherinewebs.com>
2018-04-04 06:35:24 -0700

    add sign up container with frontend route

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import GreetingContainer from './greeting/greeting_container';
 import {
   Route,
   Redirect,
@@ -7,12 +6,19 @@ import {
   Link,
   HashRouter
 } from 'react-router-dom';
-import DashboardContainer from './dashboard/dashboard_container';
+import { AuthRoute } from '../util/route_util';
+import DashboardContainer from './session/dashboard_container';
+import LoginFormContainer from './session/login_form_container';
+import SignupFormContainer from './session/sign_up_container';
 
 const App = () => (
   <div>
     <h1>clickr</h1>
-    <GreetingContainer />
+      <Switch>
+        <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        <AuthRoute exact path="/login" component={LoginFormContainer} />
+        <Route exact path="/" component={DashboardContainer} />
+      </Switch>
   </div>
 );
 
