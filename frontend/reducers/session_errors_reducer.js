9927a7cee06a68cf572045fe57864e8c46530d0f
Jonathan Lau <jonhlau28@gmail.com>
2018-04-03 20:53:45 -0700

    add redux cycle componenets, update package.json

---
+++ frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,17 @@
+import {
+  RECEIVE_SESSION_ERRORS,
+  RECEIVE_CURRENT_USER,
+} from '../actions/session_actions';
+
+
+export default (state = [], action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_SESSION_ERRORS: // set error to action's errors
+      return action.errors;
+    case RECEIVE_CURRENT_USER: // clears errors
+      return [];
+    default:
+      return state;
+  }
+};
