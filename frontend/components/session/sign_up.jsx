05b9bd3943908a9ac946a562c941ab2c3f80c66b
Jonathan Lau <jonathan.lau@catherinewebs.com>
2018-04-04 11:50:16 -0700

    add sessionContainer styling

--- frontend/components/session/sign_up.jsx
+++ frontend/components/session/sign_up.jsx
@@ -28,64 +28,73 @@ class SignupForm extends React.Component {
 
   render() {
     return (
-      <div className="session-box-body">
-        <div className="session-box">
-          <h2><p>Sign up</p></h2>
-            <form className="signup-form-challenge" onSubmit={this.handleSubmit}>
-              <fieldset className="f-l-name-container">
-                <div className="row-1-contain">
-                  <input
-                    id="row-1-input"
-                    type="text"
-                    placeholder="First name"
-                    value={this.state.fname}
-                    onChange={this.handleChange('fname')}
-                  />
-                </div>
-                <div className="row-1-2-contain">
-                  <input
-                    id="row-1-input"
-                    type="text"
-                    placeholder="Last name"
-                    value={this.state.lname}
-                    onChange={this.handleChange('lname')}
-                  />
-                </div>
-              </fieldset>
-              <div className="row-email-contain">
-                <input
-                  id="row-2-input"
-                  type="text"
-                  placeholder="Email address"
-                  value={this.state.email}
-                  onChange={this.handleChange('email')}
-                />
-              </div>
-              <div className="row-username-contain">
-                <input
-                  id="row-3-input"
-                  type="text"
-                  placeholder="Username"
-                  value={this.state.username}
-                  onChange={this.handleChange('username')}
-                />
-              </div>
-              <div className="row-password-contain">
-                <input
-                    id="row-4-input"
-                    type="password"
-                    placeholder="Password"
-                    value={this.state.password}
-                    onChange={this.handleChange('password')}
-                  />
-              </div>
-              <div className="policy-contain">
-                <p className="policy-item">By clicking "Continue", you agree to the Terms and Privacy Policy</p>
-              </div>
-             <input className="signUpButton" id="signup" type="submit" value="Sign Up"/>
-             <div className="row-sign-in">Already have an acccount? <Link to="/login"><p id="log-in-link">Sign in</p></Link></div>
-            </form>
+      <div>
+        <header className="session-new-header">
+          <div className="header-logo-contain">
+            <img className="top-logo" alt="logo-img" src="https://s.yimg.com/rz/d/yahoo_en-US_f_p_bestfit_2x.png" />
           </div>
+        </header>
+        <div className="signup-position-container">
+          <div className="session-box-body">
+            <div className="session-box">
+              <h2><p>Sign up</p></h2>
+                <form className="signup-form-challenge" onSubmit={this.handleSubmit}>
+                  <fieldset className="f-l-name-container">
+                    <div className="row-1-contain">
+                      <input
+                        id="row-1-input"
+                        type="text"
+                        placeholder="First name"
+                        value={this.state.fname}
+                        onChange={this.handleChange('fname')}
+                      />
+                    </div>
+                    <div className="row-1-2-contain">
+                      <input
+                        id="row-1-input"
+                        type="text"
+                        placeholder="Last name"
+                        value={this.state.lname}
+                        onChange={this.handleChange('lname')}
+                      />
+                    </div>
+                  </fieldset>
+                  <div className="row-email-contain">
+                    <input
+                      id="row-2-input"
+                      type="text"
+                      placeholder="Email address"
+                      value={this.state.email}
+                      onChange={this.handleChange('email')}
+                    />
+                  </div>
+                  <div className="row-username-contain">
+                    <input
+                      id="row-3-input"
+                      type="text"
+                      placeholder="Username"
+                      value={this.state.username}
+                      onChange={this.handleChange('username')}
+                    />
+                  </div>
+                  <div className="row-password-contain">
+                    <input
+                        id="row-4-input"
+                        type="password"
+                        placeholder="Password"
+                        value={this.state.password}
+                        onChange={this.handleChange('password')}
+                      />
+                  </div>
+                  <div className="policy-contain">
+                    <p className="policy-item">By clicking "Continue", you agree to the Terms and Privacy Policy</p>
+                  </div>
+                 <input className="signUpButton" id="signup" type="submit" value="Sign Up"/>
+                 <div className="row-sign-in">Already have an acccount? <Link to="/login"><p id="log-in-link">Sign in</p></Link></div>
+                </form>
+              </div>
+          </div>
+        </div>
       </div>
     );
   }
