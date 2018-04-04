9fba31ba6eeae307bf7d479a9fa35b0cccea0472
Jonathan Lau <jonhlau28@gmail.com>
2018-04-04 10:15:09 -0700

    add signupcomponent styling. todo: error rending for sessioncontainers

--- frontend/components/session/sign_up.jsx
+++ frontend/components/session/sign_up.jsx
@@ -31,42 +31,58 @@ class SignupForm extends React.Component {
       <div className="session-box-body">
         <div className="session-box">
           <h2><p>Sign up</p></h2>
-            <form onSubmit={this.handleSubmit}>
-              <div className="first-name-contain">
+            <form className="signup-form-challenge" onSubmit={this.handleSubmit}>
+              <fieldset className="f-l-name-container">
+                <div className="row-1-contain">
+                  <input
+                    id="row-1-input"
+                    type="text"
+                    placeholder="First name"
+                    value={this.state.fname}
+                    onChange={this.handleChange('fname')}
+                  />
+                </div>
+                <div className="row-1-2-contain">
+                  <input
+                    id="row-1-input"
+                    type="text"
+                    placeholder="Last name"
+                    value={this.state.lname}
+                    onChange={this.handleChange('lname')}
+                  />
+                </div>
+              </fieldset>
+              <div className="row-email-contain">
                 <input
+                  id="row-2-input"
                   type="text"
-                  placeholder="First name"
-                  value={this.state.fname}
-                  onChange={this.handleChange('fname')}
+                  placeholder="Email address"
+                  value={this.state.email}
+                  onChange={this.handleChange('email')}
                 />
               </div>
-              <div className="last-name-contain">
+              <div className="row-username-contain">
                 <input
+                  id="row-3-input"
                   type="text"
-                  placeholder="Last name"
-                  value={this.state.lname}
-                  onChange={this.handleChange('lname')}
+                  placeholder="Username"
+                  value={this.state.username}
+                  onChange={this.handleChange('username')}
                 />
               </div>
-            <input
-              type="text"
-              placeholder="Email"
-              value={this.state.email}
-              onChange={this.handleChange('email')}
-            />
-            <input
-              type="text"
-              placeholder="Username"
-              value={this.state.username}
-              onChange={this.handleChange('username')}
-            />
-            <input
-                type="password"
-                placeholder="Password: Minimum 6 characters."
-                value={this.state.password}
-                onChange={this.handleChange('password')}
-              />
-             <input type="submit" value="Sign Up"/>
+              <div className="row-password-contain">
+                <input
+                    id="row-4-input"
+                    type="password"
+                    placeholder="Password"
+                    value={this.state.password}
+                    onChange={this.handleChange('password')}
+                  />
+              </div>
+              <div className="policy-contain">
+                <p className="policy-item">By clicking "Continue", you agree to the Terms and Privacy Policy</p>
+              </div>
+             <input className="signUpButton" id="signup" type="submit" value="Sign Up"/>
              <div className="row-sign-in">Already have an acccount? <Link to="/login"><p id="log-in-link">Sign in</p></Link></div>
             </form>
           </div>
