68a6a84e71e6fe5bd20c371db2d50fd6b721e439
Jonathan Lau <jonhlau28@gmail.com>
2018-04-05 09:42:54 -0700

    add sessionNav and integrate into splash container logic

--- frontend/components/splash/nav_bar.jsx
+++ frontend/components/splash/nav_bar.jsx
@@ -6,29 +6,16 @@ class NavBar extends React.Component {
     super(props);
   }
 
-  handleButtonLayout() {
-    if(this.props.currentUser) {
-      console.log("we have a user");
-      return (
-        <button className="tempLogOutButton" onClick={this.props.logout}>Log Out</button>
-      );
-    } else {
-      return(
-        <ul className="right-btn-list">
-          <li className="login-btn"><Link to='/login'>Log In</Link></li>
-          <li className="signup-btn"><Link to='/signup'>Sign Up</Link></li>
-        </ul>
-     );
-    }
-  }
-
   render() {
     return (
       <div className="global-nav-bar">
         <ul className="left-btn-list">
           <li className="logo-img">clickr</li>
         </ul>
-        {this.handleButtonLayout()}
+        <ul className="right-btn-list">
+          <li className="login-btn"><Link to='/login'>Log In</Link></li>
+          <li className="signup-btn"><Link to='/signup'>Sign Up</Link></li>
+        </ul>
       </div>
     );
 
