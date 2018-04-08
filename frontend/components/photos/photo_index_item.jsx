76c928d165b8c2f33c208f92c2fbd8d074efb869
Jonathan Lau <jonathan.lau@catherinewebs.com>
2018-04-07 21:38:58 -0700

    add user data to render user data on photo index page. render photo index page with style. update upload icon, will need to change to font awesome due to missing sprite sheets. incomplete upload styling

--- frontend/components/photos/photo_index_item.jsx
+++ frontend/components/photos/photo_index_item.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 const PhotoIndexItem = ({photo}) => {
-  console.log(photo.photo_url);
   return (
     <li>
-      <img className="photo-index-item" src={`${photo.photo_url}`} />
+      <div className="feed-activity-card">
+        <div className="photo-index-header">
+          <ul className="photo-index-header-content">
+            <li>
+              <img className="photo-index-avatar" src={`${photo.user_avatar}`} />
+            </li>
+            <li>
+              <Link to="/feed">{photo.user_fname} {photo.user_lname}</Link>
+
+            </li>
+          </ul>
+        </div>
+        <img className="photo-index-item" src={`${photo.photo_url}`} />
+        <div className="photo-index-footer">
+          <p>{photo.title}</p>
+        </div>
+        <div className="photo-index-stats">
+          <span>clickr clickr clickr</span>
+        </div>
+      </div>
     </li>
   );
 };
