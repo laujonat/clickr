3c25c19a42611a0b61917f02961fd9d2a6331bc8
Jonathan Lau <jonathan.lau@catherinewebs.com>
2018-04-08 15:35:59 -0700

    add photostream layout, working unstyled version

---
+++ frontend/components/users/user_photostream_container.jsx
@@ -0,0 +1,18 @@
+// import { connect } from 'react-redux';
+// import UserPhotostream from './user_photostream';
+// import { fetchAllPhotos } from '../../actions/photo_actions';
+// import { photoSelector } from '../../selectors/photo_selector';
+//
+// const mapStateToProps = (state) => ({
+//   currentUser: state.session.currentUser,
+//   userPhotos: photoSelector(state)
+// });
+//
+// const mapDispatchToProps = dispatch => ({
+//   getPhotos: () => dispatch(fetchAllPhotos())
+// });
+//
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(UserPhotostream);
