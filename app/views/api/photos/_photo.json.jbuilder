69384c18475c942cb2cdf60d1f8f93b5c79e368f
Jonathan Lau <jonathan.lau@catherinewebs.com>
2018-04-09 19:54:41 -0700

    basic layout for photo show page

--- app/views/api/photos/_photo.json.jbuilder
+++ app/views/api/photos/_photo.json.jbuilder
@@ -1,2 +1,3 @@
 json.extract! photo, :id, :title, :description, :user_id
 json.photo_url asset_path(photo.photo_url.url(:original))
+json.user_id photo.user.id
--- frontend/components/photos/photo_show.jsx
+++ frontend/components/photos/photo_show.jsx
@@ -4,34 +4,44 @@ import PhotoShowNav from './photo_show_nav';
 class PhotoShow extends React.Component {
   constructor(props) {
     super(props);
+    // this.state = {
+    //   loading: true
+    // };
   }
 
-  componentDidMount() {
+  componentWillMount() {
+    // this.props.getUser(this.props.photo.user_id)
+    //   .then(() => this.setState({loading: false}));
     this.props.getPhoto(this.props.match.params.photoId);
+
   }
 
   render() {
-    console.log(this.props.photo.user_id);
+
     return(
-      <div className="show-page-wrap">
-      <div className="photo-view-modal-container">
-        <div className="main-image-wrapper">
-          <img className="main-show-image" src={`${this.props.photo.photo_url}`} />
-        </div>
-        <div className="photo-view-actions-container">
-        </div>
-      </div>
-      <div className="sub-photo-view-container">
-        <div className="sub-photo-center-content">
-          <div className="sub-left-view">
-
+        <div className="show-page-wrap">
+        <div className="photo-view-modal-container">
+          <div className="main-image-wrapper">
+            <img className="main-show-image" src={`${this.props.photo.photo_url}`} />
           </div>
-          <div className="sub-right-view">
-
+          <div className="photo-view-actions-container">
           </div>
         </div>
+        <div className="sub-photo-view-container">
+          <div className="sub-photo-center-content">
+            <div className="sub-left-view">
+              <div className="photo-attribution">
+                <img className="photo-credit-avatar" src={`${this.props.photo.user_avatar}`} />
+                <div className="attribution-info">
+                </div>
+              </div>
+            </div>
+            <div className="sub-right-view">
+
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
     );
   }
 }
