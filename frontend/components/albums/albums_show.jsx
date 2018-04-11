27e3137b512e11e552969d1b52b4a14f2a689b64
Jonathan Lau <jonhlau28@gmail.com>
2018-04-10 17:51:06 -0700

    add correct tab structure to user profile

---
+++ frontend/components/albums/albums_show.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class AlbumShow extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+
+
+  render() {
+    console.log("Welcome to the jungle");
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+export default AlbumShow;
