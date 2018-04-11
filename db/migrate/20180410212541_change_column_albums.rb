02657fe19626a3205f2343f36c147a920b429479
Jonathan Lau <jonhlau28@gmail.com>
2018-04-11 13:44:59 -0700

    removal album migrations

--- db/migrate/20180410212541_change_column_albums.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnAlbums < ActiveRecord::Migration[5.1]
-  def change
-    change_column :albums, :description, :string, :null => true
-  end
-end
--- db/migrate/20180411023446_drop_albums.rb
+++
@@ -1,5 +0,0 @@
-class DropAlbums < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :albums
-  end
-end
