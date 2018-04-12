0696658e08c644753c81bef42826d22a2986148a
Jonathan Lau <jonhlau28@gmail.com>
2018-04-12 16:40:29 -0700

    add album edit page. incomplete functionality. functional version

--- frontend/components/albums/album_edit_container.jsx
+++ frontend/components/albums/album_edit_container.jsx
@@ -1,17 +1,22 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-import { updateAlbum } from '../../actions/album_actions';
+import { fetchAlbum, updateAlbum } from '../../actions/album_actions';
+import { fetchAllPhotos } from '../../actions/photo_actions';
+import { albumSelector } from "../../selectors/album_selector";
 import AlbumEdit from './album_edit';
 
 
 const mapStateToProps = (state, ownProps) => ({
   currentUser: state.session.currentUser,
-  album: state.entities.albums[ownProps.albumId],
+  album: albumSelector(state),
   photos: state.entities.photos
 });
 
 const mapDispatchToProps = dispatch => ({
-  updateAlbum: (album, id) => dispatch(updateAlbum(album, id))
+  updateAlbum: (album, id) => dispatch(updateAlbum(album, id)),
+  fetchAlbum: id => dispatch(fetchAlbum(id)),
+  fetchAllPhotos: id => dispatch(fetchAllPhotos(id))
+
 });
 
 export default withRouter(connect(
