ba1f723011a7814c63c99b7358071dbedc636d6c
Jonathan Lau <jonhlau28@gmail.com>
2018-05-08 20:22:09 -0700

    Added footer to splash page

--- frontend/components/splash/welcome.jsx
+++ frontend/components/splash/welcome.jsx
@@ -5,14 +5,25 @@ class Welcome extends React.Component {
 
   render() {
     return (
+      <React.Fragment>
       <div className="welcome-text">
         <h1 id="welcome-h1">A Flickr remake.</h1>
         <h3 id="welcome-h3">
-          Find your inspiration. A React remake of Flickr. 
+          Find your inspiration. A React remake of Flickr.
         </h3>
         <div className="welcome-btn"><Link to="/signup">Sign up</Link></div>
-
       </div>
+      <div className="welcome-footer">
+        <ul className="welcome-footer-list">
+          <li>
+            <a href="http://github.com/laujonat">Github</a>
+          </li>
+          <li>
+            <a href="http://linkedin.com/in/jonathanhlau">Linkedin</a>
+          </li>
+        </ul>
+      </div>
+      </React.Fragment>
     );
   }
 }
